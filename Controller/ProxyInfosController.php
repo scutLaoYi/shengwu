59e5cccf1600b2585843a089b3ea9b5a7837a927
shengwu004 <shengwu004@shengwu004-Inspiron-660.(none)>
2013-11-22 19:48:01 +0800

    version 20131122 19:45 by lpp001  完善代理提交和编辑

--- Controller/ProxyInfosController.php
+++ Controller/ProxyInfosController.php
@@ -13,6 +13,7 @@ class ProxyInfosController extends AppController {
  *
  * @var array
  */
+	public $helpers = array('Html','Form');
 	public $uses = array('CompanyUserInfo','ProxyInfo');
 	public $components = array('Paginator','List');
 
@@ -109,43 +110,37 @@ class ProxyInfosController extends AppController {
 		return $this->redirect(array('action' => 'index'));
 	}
 
-	public function proxy_submit()
+	public function proxy_submit($proxy_id=null)
 	{
 		$allCountrys=$this->List->allCountry();
 		$this->set('allCountrys',$allCountrys);
 		$this->set('allMaterial',$this->List->allMaterial());
 		$this->set('allProduct',$this->List->allProduct());
-		if($this->request->is('post'))
+		if($this->request->is(array('post','put')))
 		{
 			$company=$this->CompanyUserInfo->find('first',array('conditions'=>array('CompanyUserInfo.user_id'=>$this->Auth->user('id'))));
 			if($company!=null)
 			{
 				$this->request->data['ProxyInfo']['company_user_info_id']=$company['CompanyUserInfo']['id'];
 				$this->request->data['ProxyInfo']['status']='1';
-				$file = $this->data['ProxyInfo']['proxy_image'];
+				$file = $this->request->data['ProxyInfo']['picture_url'];
 				if($this->request->data['ProxyInfo']['product_type']!='3')
 					$this->request->data['ProxyInfo']['material']='0';
-				else if($this->request->data['ProxyInfo']['material']=='0')
-				{
-					$this->Session->setFlash('表单提交失败，请选择该产品所属卫生材料分类！');
-					return ;
-				}
-				print_r($this->data['ProxyInfo']['product_type']);
-				print_r($this->data['ProxyInfo']['function']);
-				print_r($this->data['ProxyInfo']['department']);
-				print_R($this->data['ProxyInfo']['material']);
 				$ext=substr(strtolower(strrchr($file['name'],'.')),1);
 				$arr_ext=array('jpg','jpeg','gif','png');
 				$path=$this->Auth->user('username').'_'.date("YmdHis").'.'.$ext;
 				if(in_array($ext,$arr_ext))
 				{
-					move_uploaded_file($file['tmp_name'],WWW_ROOT.'img/proxy_image/'.$path);
+					move_uploaded_file($file[ 'tmp_name'],WWW_ROOT.'img/proxy_image/'.$path);
 					$this->request->data['ProxyInfo']['picture_url']=$path;
 				}
+				else
+					$this->request->data['ProxyInfo']['picture_url']=null;
 				if($this->ProxyInfo->save($this->request->data))
 				{
+					print_r($this->request->data);
 					$this->Session->setFlash('代理信息已提交，等待管理员审核');
-					
+					return $this->redirect(array('controller'=>'Mainpage','action'=>'index'));
 				}
 				else
 				{
@@ -158,12 +153,37 @@ class ProxyInfosController extends AppController {
 				$this->Session->setFlash('您当前不是企业用户，不能发布代理信息');
 			}
 		}
+		else
+		{
+			if($proxy_id!=null)
+			{
+			$company=$this->CompanyUserInfo->find('first',array('conditions'=>array('CompanyUserInfo.user_id'=>$this->Auth->user('id'))));
+			$proxy=$this->ProxyInfo->find('first',array('conditions'=>array('ProxyInfo.id'=>$proxy_id)));
+			if($company!=null&&$proxy!=null&&$company['CompanyUserInfo']['id']==$proxy['ProxyInfo']['company_user_info_id'])
+			{
+			$this->request->data=$proxy;
+			$this->set('allFunction',$this->List->allFunction($this->request->data['ProxyInfo']['product_type']));
+			$this->set('allDepartment',$this->List->allDepartment($this->request->data['ProxyInfo']['product_type']));
+
+			}
+			else
+			{
+			$this->Session->setFlash('您不是该代理信息拥有者，不能编辑该代理信息');
+			return $this->redirect(array('controller'=>'Mainpage','action'=>'index'));
+			}
+				
+
+
+			}
+			
+		}
 	}
+
 	public function isAuthorized($user)
 	{
 		if($this->Auth->user('type')==1)
 		{
-			if(in_array($this->action,array('proxy_submit')))
+			if(in_array($this->action,array('proxy_submit','proxy_edit')))
 			{
 				return true;
 			}
