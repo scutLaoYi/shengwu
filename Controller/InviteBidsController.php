aa3eb066dc2dd9cd9ade8d7c528ec031d3addac0
shengwu004 <shengwu004@shengwu004-Inspiron-660.(none)>
2013-12-03 14:52:10 +0800

    version 20131203 14:50 by lpp001 增加bake InviteBids

--- Controller/InviteBidsController.php
+++ Controller/InviteBidsController.php
@@ -1,8 +1,103 @@
 <?php
+App::uses('AppController', 'Controller');
+/**
+ * InviteBids Controller
+ *
+ * @property InviteBid $InviteBid
+ * @property PaginatorComponent $Paginator
+ */
+class InviteBidsController extends AppController {
 
-class InviteBidsController extends AppController
-{
-	public $scaffold;
-}
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
 
-?>
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->InviteBid->recursive = 0;
+		$this->set('inviteBids', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->InviteBid->exists($id)) {
+			throw new NotFoundException(__('Invalid invite bid'));
+		}
+		$options = array('conditions' => array('InviteBid.' . $this->InviteBid->primaryKey => $id));
+		$this->set('inviteBid', $this->InviteBid->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->InviteBid->create();
+			if ($this->InviteBid->save($this->request->data)) {
+				$this->Session->setFlash(__('The invite bid has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The invite bid could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->InviteBid->exists($id)) {
+			throw new NotFoundException(__('Invalid invite bid'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->InviteBid->save($this->request->data)) {
+				$this->Session->setFlash(__('The invite bid has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The invite bid could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('InviteBid.' . $this->InviteBid->primaryKey => $id));
+			$this->request->data = $this->InviteBid->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->InviteBid->id = $id;
+		if (!$this->InviteBid->exists()) {
+			throw new NotFoundException(__('Invalid invite bid'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->InviteBid->delete()) {
+			$this->Session->setFlash(__('The invite bid has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The invite bid could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}}
